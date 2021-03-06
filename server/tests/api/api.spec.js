001248274a7c4ad883cd9e7a733c870d1bcf90e1
chris westerduin <chriswesterduin@outlook.com>
2021-03-06 10:31:55 +0000

    add starter api tests

---
+++ server/tests/api/api.spec.js
@@ -0,0 +1,27 @@
+// import server
+const server = require('../../server');
+const supertest = require('supertest');
+const request = supertest(server);
+
+
+describe('Get routes', () => {
+    let api
+    beforeAll(() => {
+        // start the server and store it in the api variable
+        api = server.listen(5000, () =>
+            console.log('Test server running on port 5000')
+        );
+    });
+    afterAll((done) => {
+        // close the server, then run done
+        console.log('Gracefully stopping test server');
+        api.close(done);
+    });
+
+    it('Responds with 200', (done) => {
+      request.get('/').expect(200, done);
+    })
+    it('responds to non existing paths with 404', (done) => {
+      request.get('/no').expect(404, done);
+    });
+})
---
+++ server/tests/api/pollRoutes.spec.js
@@ -0,0 +1,30 @@
+// import server
+const server = require('../../server');
+const supertest = require('supertest');
+const request = supertest(server);
+
+let testPoll = {
+    id: 1,
+    name: 'poll123'
+}
+
+describe('Test for poll specific routes', () => {
+    
+    it('Responds with 200 to /polls', (done) => {
+      request.get('/polls').expect(200, done);
+    })
+
+    describe('Get specific poll', () => {
+        it('Responds to request for specific poll with 200', (done) => {
+            request.get('/polls/1').expect(200, done);
+        })
+        it('Responds to request for specific poll with poll data', (done) => {
+            request.get('/polls/1').expect({
+                id: 1,
+                name: 'poll123'
+            })
+        }, done)
+    })
+
+
+})
